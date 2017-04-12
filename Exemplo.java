d953844abc3d71991135daf26c84b9fc3cb16ab2
Luiz Felipe Souza <luizsci42@gmail.com>
2017-04-12 11:52:39 -0300

    Fazendo uso do valor digitado pelo usuário em um JOptionPane

--- Exemplo.java
+++ Exemplo.java
@@ -1,20 +1,23 @@
 package gui;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.GradientPaint;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.InputStream;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class Exemplo implements ActionListener {
+public class Exemplo implements ActionListener, MouseListener {
 
 	private JFrame frame;
+	private String s;
+	private int raio;
+	private double area;
 
 	public static void main(String[] args) {
 		Exemplo ex = new Exemplo();
@@ -24,22 +27,61 @@ public class Exemplo implements ActionListener {
 	private void init() {
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.addMouseListener(this);
+
+		JButton circulo = new JButton("Círculo");
 		
-		JButton button = new JButton("Mudar cores");
-		button.addActionListener(this);
+		circulo.addActionListener(this);
 
-		Painel p = new Painel();
+		Painel painel = new Painel();
 
-		frame.getContentPane().add(BorderLayout.SOUTH, button);
-		frame.getContentPane().add(BorderLayout.CENTER, p);
+		frame.getContentPane().add(BorderLayout.SOUTH, circulo);
+		frame.getContentPane().add(BorderLayout.CENTER, painel);
 		frame.setSize(400, 400);
 		frame.setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		InputStream in = System.in;
+		s = JOptionPane.showInputDialog(frame, "Digite o raio do círculo", in);
+		raio = Integer.parseInt(s);
+		area = Math.PI * (raio*raio);
+		System.out.println("Área: " + area);
 		frame.repaint();
+	}
 
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// falta dizer que o frame é um mouse listener
+		int x = e.getX();
+		int y = e.getY();
+		frame.repaint(x, y, raio, raio);
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
 
@@ -54,9 +96,8 @@ class Painel extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
-		Graphics2D g2d = (Graphics2D) g;
-
-		int red = (int) (Math.random() * 255);
+		/*
+		int red = (int) (Math.random() * 255); 
 		int green = (int) (Math.random() * 255);
 		int blue = (int) (Math.random() * 255);
 
@@ -71,6 +112,8 @@ class Painel extends JPanel {
 		GradientPaint gradient = new GradientPaint(70, 70, c1, 150, 150, c2);
 		g2d.setPaint(gradient);
 		g2d.fillOval(70, 70, 100, 100);
+		
+		*/
 	}
 
 }
